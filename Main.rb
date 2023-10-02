784420da36a1800f9bf89da4e510f265a0f4cbb7
Joe Pettit <Joe.pettit91@gmail.com>
2023-10-02 13:53:51 -0700

    needs to be formatted in vscode or something

--- Main.rb
+++ Main.rb
@@ -1,44 +1,51 @@
+# frozen_string_literal: true
+
 load 'user.rb'
 load 'menu.rb'
 load 'loan.rb'
 load 'calculator.rb'
 
 user = User.new
-calc = Calculator.new 
+calc = Calculator.new
 
-main_menu = Menu.new("WELCOME")
-main_menu.add_menu_item("New Loan")
-main_menu.add_menu_item("Show Loans")
-main_menu.add_menu_item("Amortization")
+main_menu = Menu.new('WELCOME')
+main_menu.add_menu_item('New Loan')
+main_menu.add_menu_item('Show Loans')
+main_menu.add_menu_item('Amortization')
 
-while main_menu.display != -1
-    case main_menu.option 
-    when 1 
-        loan_menu = Menu.new("loans")
-        loan_menu.add_menu_item("Auto")
-        loan_menu.add_menu_item("Mortgage")
-        loan_menu.add_menu_item("Personal")
-        
-        case loan_menu.display
-        when 1 
-            loan = Auto.new
-        when 2 
-            loan = Mortgage.new
-        when 3
-            loan = Personal.new
-        end 
-        
-        if loan_menu.option != -1 
-            loan.load_values
-            loan.payment = calc.monthly_payment(loan)
-            user.loans.push(loan)
-        end
 
-    when 2 
-        main_menu.show_loan_table(user.loans)
-    when 3 
-        selection = main_menu.show_loan_table(user.loans, selection = true)
-        calc.amortization_table(user.loans[selection])
-    end 
-end 
-        
+loop do
+case main_menu.display
+ when 1
+    loan_menu = Menu.new('loans')
+    loan_menu.add_menu_item('Auto')
+    loan_menu.add_menu_item('Mortgage')
+    loan_menu.add_menu_item('Personal')
+
+    case loan_menu.display
+    when 1
+      loan = Auto.new
+    when 2
+      loan = Mortgage.new
+		when 3
+		 loan = Personal.new
+    else
+		 loan = nil
+		end
+
+		if loan
+    	loan.load_values
+    	loan.payment = calc.monthly_payment(loan)
+    	user.loans.push(loan)
+		end
+
+ when 2
+    main_menu.show_loan_table(user.loans)
+ when 3
+    selection = main_menu.show_loan_table(user.loans, selection: true)
+		calc.amortization_table(user.loans[selection - 1]) if selection != -1
+ else
+	main_menu.show_header("GOODBYE")
+	break
+ end
+end
--- menu.rb
+++ menu.rb
@@ -1,84 +1,96 @@
+# frozen_string_literal: true
+
 #    Menu:
 #    A menu class which is used to display the menu.
 #    User input will be used to navigate the menu.
 class Menu
-    attr_accessor :option
+  attr_accessor :option
 
-    def initialize(name)
-        @option = -1
-        @width = 60;
-        @h_char = "-"
-        @v_char = "|"
-        @items = []
-        @name = name
+  def initialize(name)
+    @option = -1
+    @width = 60
+    @h_char = '-'
+    @v_char = '|'
+    @items = []
+    @name = name
+  end
+
+  def display
+    show_header(@name)
+    @items.each_with_index do |item, index|
+      show_menu_item(index + 1, item)
+    end
+    # show_menu_item(-1, 'exit')
+    show_footer
+    @option = validate_menu_selection
+  end
+
+  def add_menu_item(item)
+    @items.push(item)
+  end
+
+  def show_loan_table(loans, selection: false)
+    if loans.empty?
+      show_header('no loans')
+      print 'Any key to return to menu '
+      gets
+      return
     end
 
-    def display
-        show_header(@name)
-        @items.each_with_index do |item, index|
-            show_menu_item(index + 1, item)
-        end
-        show_menu_item(-1, "exit");
-        show_footer
-        print "Enter selection: "
-        @option = gets.chomp.to_i
-        return option
-    end 
-
-    def add_menu_item(item)
-        @items.push(item)
-    end 
-
-    def show_loan_table(loans, selection = false)
-
-        if loans.length == 0
-            show_header("no loans")
-            print "Any key to return to menu "
-            gets
-            return
-        end
-
-        show_header("loans")
-        puts "| %-10s %12s %9s %7s %8s" % ["Loan type", "Principal", "Interest", "Months", "Payment"]
-        loans.each_with_index do |item, index| 
-            line = "| %d. %-10s %9.2f %10.2f %6d $%.2f" % [index + 1, item.class, item.amount, item.interest, item.term, item.payment]
-            offset = @width - line.length
-            right = "%#{offset -1}s" % [@v_char]
-            puts line + right
-        end 
-        show_footer
-        if selection
-            print "Enter line number to select loan"
-            return (gets.chomp.to_i - 1)
-        else 
-            print "Any key to return to menu "
-            gets
-        end 
+    show_header('loans')
+    puts format('| %-10s %12s %9s %7s %8s', 'Loan type', 'Principal', 'Interest', 'Months', 'Payment')
+    loans.each_with_index do |item, index|
+      line = format('| %d. %-10s %9.2f %10.2f %6d $%.2f', index + 1, item.class, item.amount, item.interest, item.term,
+                    item.payment)
+      offset = @width - line.length
+      right = format("%#{offset - 1}s", @v_char)
+      puts line + right
     end
 
-    def show_amortization()
-        calc = Calculator.new
-        calc.calc_amortization_table(@user.loans.first)
-    end 
+    show_footer
+    validate_menu_selection(length: loans.length, selection:selection)
+  end
 
-    # Displays a menu header with centered text 
-    def show_header(text)
-        bar = @h_char * @width
-        offset = (@width - text.length) / 2
-        left = "%#{offset}s" % [@v_char]
-        right = "%-#{offset}s" % [@v_char]
-        puts "\n" + bar + "\n" + right + text.upcase + left + "\n" + bar
-    end 
 
-    # Displays menu item 
-    def show_menu_item(index, text)
-        right = "| %d. " % [index]
-        offset = ((@width / 2) - text.length) + ((@width / 2) - right.length)
-        left = "%#{offset}s" % [@v_char]
-        puts right + text + left
-    end 
+  # Displays a menu header with centered text
+  def show_header(text)
+    bar = @h_char * @width
+    offset = (@width - text.length) / 2
+    left = format("%#{offset}s", @v_char)
+    right = format("%-#{offset}s", @v_char)
+    puts "\n#{bar}\n#{right}#{text.upcase}#{left}\n#{bar}"
+  end
 
-    def show_footer
-        puts @h_char * @width + "\n"
+  # Displays menu item
+  def show_menu_item(index, text)
+    right = format('| %d. ', index)
+    offset = ((@width / 2) - text.length) + ((@width / 2) - right.length)
+    left = format("%#{offset}s", @v_char)
+    puts right + text + left
+  end
+
+  def show_footer
+    puts "#{@h_char * @width}\n"
+  end
+
+  def validate_menu_selection(length:nil, selection:true)
+    if length.nil?
+      length = @items.length
     end
+
+    if selection
+      print 'Enter selection, -1 to quit: '
+    else
+      print 'Any key to continue '
+    end
+
+    option = gets.chomp.to_i
+    while !selection and (option < 0 or option > length) and option != -1
+      print "\n invalid selection \n"
+      print "Enter selection, -1 to quit: "
+      option = gets.chomp.to_i
+    end
+    option = option
+  end
 end
+
